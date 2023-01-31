8a3ea7db1b434643a7d11c474a06ec4f694da049
MH-Bidhan <mdbidhan55@gmail.com>
2023-01-31 08:27:14 +0600

    Defined a function to generate a deck pf cards

---
+++ utils/generateCards.js
@@ -0,0 +1,28 @@
+function generateCards() {
+  const suits = ["spades", "hearts", "clubs", "diamonds"];
+  const map = {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    king: 10,
+    queen: 10,
+    jack: 10,
+    ace: 11,
+  };
+
+  const cards = [];
+
+  for (suit of suits) {
+    for (key in map) {
+      cards.push({ svg: `${suit}_${key}.svg`, point: map[key] });
+    }
+  }
+
+  return cards;
+}
