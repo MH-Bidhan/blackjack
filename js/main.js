ac3739afb6e7e34efb2171918128e18f996551dd
Mahamud Hassan <mdbidhan55@gmail.com>
2023-02-01 00:04:59 +0600

    Modified the app to deal cards at the begaining of the game and to add the ability for user to hit for a new card

--- js/main.js
+++ js/main.js
@@ -1,6 +1,38 @@
+import hitNewCard from "./../helper/hitNewCard.js";
+
+const dealtCards = {};
+
+const user = {
+  name: "player",
+  cards: [],
+  points: 0,
+};
+
+const computer = {
+  name: "computer",
+  cards: [],
+  points: 0,
+};
+
 $(document).ready(function () {
   // Function to close the lobby window.
   $("#btn-play").click(function () {
     $("#lobby").remove();
+
+    // Start the game by dealing 2 cards on each side.
+    const dealCards = setInterval(() => {
+      if (user.cards.length === 2 && computer.cards.length === 2)
+        return clearInterval(dealCards);
+      if (user.cards.length < 2) {
+        hitNewCard(user, dealtCards);
+      }
+
+      if (computer.cards.length < 2) {
+        hitNewCard(computer, dealtCards);
+      }
+    }, 800);
   });
+
+  // Function for user to hit for a card
+  $("#btn-hit").click(() => hitNewCard(user, dealtCards));
 });
