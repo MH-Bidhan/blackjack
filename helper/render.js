c7c4f0865c05b321031b5268e1ae75af8a7cbba2
MH-Bidhan <mdbidhan55@gmail.com>
2023-02-01 12:42:21 +0600

    Defined functions to render result and reveal the hidden card

--- helper/render.js
+++ helper/render.js
@@ -1,5 +1,5 @@
 function renderNewCard(name, newCard) {
-  $(`#${name}-card-container`).append(
+  return $(`#${name}-card-container`).append(
     `<div class="card-slot"  ${
       newCard.hide === true ? 'id= "card-hide"' : ""
     }><img src="cards/${newCard.svg}"  alt="" /></div>`
@@ -12,4 +12,27 @@ function renderPoints(player) {
   return $(`#point-${name}`).text(points);
 }
 
-export { renderNewCard, renderPoints };
+function revealHiddenCard() {
+  return $("#card-hide").removeAttr("id");
+}
+
+function renderResult({ won = false, draw = false, blackjack = false }) {
+  if (draw) return $(`#result`).text("Draw");
+
+  if (blackjack) {
+    revealHiddenCard();
+    $(`#blackjack`).text("Blcakjack");
+  } else {
+    $(`#blackjack`).hide();
+  }
+
+  if (won) {
+    $(`#result`).text("You Won");
+  } else {
+    $(`#result`).text("You Lose");
+  }
+
+  return $("#result-board").show();
+}
+
+export { renderNewCard, renderPoints, revealHiddenCard, renderResult };
