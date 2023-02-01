8ecedf6227d26e5731d0a8fa4067a8f79cd82a07
Mahamud Hassan <mdbidhan55@gmail.com>
2023-02-01 21:06:29 +0600

    Added comments on the main app file and refacored some codes

--- js/main.js
+++ js/main.js
@@ -15,23 +15,24 @@ const computer = {
 };
 
 $(document).ready(function () {
-  // Function to close the lobby window.
+  // Function to close the lobby window and start a game when the play button is clicked.
   $("#btn-play").click(function () {
     $("#lobby").remove();
     startNewGame(user, computer, dealtCards);
   });
 
-  // Function for user to hit for a card
-
+  // Function for user to hit for a card by clicking hit button.
   $("#btn-hit").click(() => {
     if (computer.cards.length >= 2 && user.cards.length >= 2)
       hitNewCard(user, dealtCards);
   });
+
+  // Function to start a new game when new game button is clicked.
   $("#btn-new-game").click(() => {
     dealtCards = {};
     startNewGame(user, computer, dealtCards);
   });
 
-  // Function for the computer to play if user stands
+  // Function for the computer to play if user stands when the stand button is clicked.
   $("#btn-stand").click(() => stand(user, computer, dealtCards));
 });
