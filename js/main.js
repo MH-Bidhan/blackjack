245cfa8ccb945777f319d7a470da63b018d737ac
MH-Bidhan <mdbidhan55@gmail.com>
2023-02-01 12:44:17 +0600

    Refactored main.js files codes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import { renderPoints } from "../helper/render.js";
-import hitNewCard from "./../helper/hitNewCard.js";
+import { hitNewCard, stand, startNewGame } from "../helper/blackjack.js";
 
 const dealtCards = {};
 
@@ -19,40 +18,17 @@ $(document).ready(function () {
   // Function to close the lobby window.
   $("#btn-play").click(function () {
     $("#lobby").remove();
-
-    // Start the game by dealing 2 cards on each side.
-    const dealCards = setInterval(() => {
-      if (user.cards.length === 2 && computer.cards.length === 2)
-        return clearInterval(dealCards);
-      if (user.cards.length < 2) {
-        hitNewCard(user, dealtCards);
-      }
-
-      if (computer.cards.length < 2) {
-        hitNewCard(computer, dealtCards);
-      }
-    }, 800);
+    startNewGame(user, computer, dealtCards);
   });
 
   // Function for user to hit for a card
-  $("#btn-hit").click(() => hitNewCard(user, dealtCards));
+
+  $("#btn-hit").click(() => {
+    if (computer.cards.length >= 2 && user.cards.length >= 2)
+      hitNewCard(user, dealtCards);
+  });
+  $("#btn-new-game").click(() => startNewGame(user, computer, dealtCards));
 
   // Function for the computer to play if user stands
-  $("#btn-stand").click(() => {
-    computer.cards.forEach((card) =>
-      card.hide ? (computer.points += card.point) : null
-    );
-    renderPoints(computer);
-    $("#card-hide").removeAttr("id");
-
-    const computerDraw = setInterval(() => {
-      if (user.points > 18 && user.points > computer.points) {
-        hitNewCard(computer, dealtCards);
-      } else if (user.points <= 18 && user.points >= computer.points) {
-        hitNewCard(computer, dealtCards);
-      } else {
-        clearInterval(computerDraw);
-      }
-    }, 800);
-  });
+  $("#btn-stand").click(() => stand(user, computer, dealtCards));
 });
